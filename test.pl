f191c2efd2f54d902d10bcc790cbec7a360d0d50
Nick-Desktop <nicholas.zimmermann@gmail.com>
2018-05-08 21:55:31 -0700

    .

--- test.pl
+++ test.pl
@@ -10,23 +10,36 @@ person(clay, 27).
 person(bob, 25).
 person(zack, 21).
 person(will, 20).
-gender(erika, female).
-gender(christine, female).
-gender(tori, female).
-gender(sydney, female).
-gender(maria, female).
-gender(nick, male).
-gender(clay, male).
-gender(bob, male).
-gender(zack, male).
-gender(will, male).
+likes(will, cars).
+likes(will, books).
+likes(nick, videogames).
+likes(nick, work).
+likes(christine, work).
+likes(christine, books).
+likes(erika, clubs).
+likes(erika, bars).
+likes(tori, clubs).
+likes(tori, drinking).
+likes(sydney, drinking).
+likes(sydney, clubs).
+likes(clay, videogames).
+likes(clay, books).
+likes(maria, books).
+likes(maria, videogames).
+likes(bob, golfing).
+likes(bob, books).
+likes(zack, videogames).
+likes(zack, airsoft).
+likes(will, videogames).
+likes(will, airsoft).
 
 
-/* People are compatible if their age is the same., they are not the same person, and they are not the same gender. */
+/* People are compatible if their age is the same., they are not the same person, and they like the same thing */
 
 compatible(PersonA, PersonB) :- 
 person(PersonA, X),
 person(PersonB, X), 
 PersonA \= PersonB,
-gender(PersonA, Z), gender(personB,Z).
+likes(personA,Y),
+likes(personB,Y).
  
