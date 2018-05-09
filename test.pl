b90e05d2ed6983214c2605721ceb360ab6eb6dc2
Nick-Desktop <nicholas.zimmermann@gmail.com>
2018-05-08 22:02:37 -0700

    test

--- test.pl
+++ test.pl
@@ -11,30 +11,19 @@ person(bob, 25).
 person(zack, 21).
 person(will, 20).
 likes(will, cars).
-likes(will, books).
 likes(nick, videogames).
-likes(nick, work).
 likes(christine, work).
-likes(christine, books).
 likes(erika, clubs).
-likes(erika, bars).
-likes(tori, clubs).
 likes(tori, drinking).
 likes(sydney, drinking).
-likes(sydney, clubs).
 likes(clay, videogames).
-likes(clay, books).
-likes(maria, books).
 likes(maria, videogames).
 likes(bob, golfing).
-likes(bob, books).
 likes(zack, videogames).
-likes(zack, airsoft).
 likes(will, videogames).
-likes(will, airsoft).
 
 
-/* People are compatible if their age is the same., they are not the same person, and they like the same thing */
+/* People are compatible if their age is the same, they are not the same person, and they like the same thing */
 
 compatible(PersonA, PersonB) :- 
 person(PersonA, X),
