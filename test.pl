63ffb56cf5e8113fa1e6a5078716d7a5f0aaf2c6
Nick-Desktop <nicholas.zimmermann@gmail.com>
2018-05-08 20:42:50 -0700

    Changed the prolog program to a different example.

--- test.pl
+++ test.pl
@@ -1,8 +1,18 @@
-cost(cornflakes, 2).
-cost(coffee, 3).
-cost(chocolate, 5).
-cost(crisps, 12).
-total_cost([Item|Rest], Cost) :-
- cost(Item, ItemCost),
- total_cost(Rest, CostOfRest),
- Cost is ItemCost + CostOfRest.
+age(erika, 21). /* facts */
+age(christine, 22).
+age(tori, 20).
+age(sydney, 25).
+age(maria, 22).
+age(nick, 22).
+age(clay, 27).
+age(bob, 25).
+age(zack, 21).
+age(will, 20).
+
+/* People are compatible if their age is the same. */
+
+compatible([],0).
+compatible(Name, Age) :-
+age(X, Y),
+age(Z, Y).
+ 
